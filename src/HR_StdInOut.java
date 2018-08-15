f51aecf7eca9e56c98d1ef5f61af0fab44695791
Jay Teli <jay.t@pepperfry.com>
2018-08-15 19:19:23 +0530

    updated

--- src/HR_StdInOut.java
+++ src/HR_StdInOut.java
@@ -3,8 +3,24 @@ import java.io.InputStreamReader;
 
 public class HR_StdInOut {
 	public static void main(String[] args) {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			try {
+				HR_StdInOut main = new HR_StdInOut();
+				int n1 = Integer.parseInt(br.readLine());
+				int n2 = Integer.parseInt(br.readLine());
+				int n3 = Integer.parseInt(br.readLine());
+				System.out.println(n1);
+				System.out.println(n2);
+				System.out.print(n3);
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				br.close();
+			}
+		} catch (Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }
