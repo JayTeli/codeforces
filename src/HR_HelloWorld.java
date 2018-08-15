7a11514d32f54d96886b5d40ec4234bd79184036
Jay Teli <jay.t@pepperfry.com>
2018-08-15 20:19:15 +0530

    fixes

--- src/HR_HelloWorld.java
+++ src/HR_HelloWorld.java
@@ -2,7 +2,7 @@
 /**
  *	https://www.hackerrank.com/challenges/welcome-to-java/problem
  */
-public class HelloWorld {
+public class HR_HelloWorld {
 
     public static void main(String[] args) {
         System.out.println("Hello, World.");
