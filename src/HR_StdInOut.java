9ef92a85a25ab231b3a26b9c2f49f0df7b4040cc
Jay Teli <jay.t@pepperfry.com>
2018-08-15 18:23:11 +0530

    StdIO

--- src/HR_StdInOut.java
+++ src/HR_StdInOut.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
-public class StdInOut {
+public class HR_StdInOut {
 	public static void main(String[] args) {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
